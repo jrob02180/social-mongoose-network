16421289a8b8d70f441710f159e73107766a35a3
jrob02180 <jrob02180@gmail.com>
2023-04-29 12:51:58 -0600

    Added seed and data utils

---
+++ utils/data.js
@@ -0,0 +1,103 @@
+const names = [
+    'Aaran',
+    'Aaren',
+    'Aarez',
+    'Aarman',
+    'Aaron',
+    'Aaron-James',
+    'Aarron',
+    'Aaryan',
+    'Aaryn',
+    'Zubayr',
+    'Zuriel',
+    'Xander',
+    'Jared',
+    'Courtney',
+    'Gillian',
+    'Clark',
+    'Jared',
+    'Grace',
+    'Kelsey',
+    'Tamar',
+    'Alex',
+    'Mark',
+    'Tamar',
+    'Farish',
+    'Sarah',
+    'Nathaniel',
+    'Parker',
+  ];
+  
+  const thoughtTexts = [
+
+    'If the implementation is hard to explain, it\'s a bad idea.',
+    'If the implementation is easy to explain, it may be a good idea.',
+    'Namespaces are one honking great idea -- let\'s do more of those!',
+    'Be kind to yourself.',
+    'Here\'s a cool thought',
+    'You have the potential to make beautiful things. Yes, you.',
+    'You are learning valuable skills right now.',
+    'You are worthy and deserving of respect.',
+    'You are worthy of love.',
+    'You are enough.',
+    'You are capable of anything.',
+    'You are stronger than you think.',
+    'You are not alone.',
+    'Empty yourself and let the universe fill you.',
+    'You are not your thoughts.',
+    'Stop looking behind you, you\'re not going that way.',
+  ]
+
+    const reactions = [
+     'Love this!',
+        'OMG',
+        'LOL',
+        'So true',
+        'Haha',
+        'Wow',
+        'Ok!',
+        'I don\'t agree',
+        'I agree',
+        'I disagree',
+        'I\'m not sure',
+        'I\'m sure',
+        'Cool idea',
+        'Great thought',
+    ];
+
+    const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+    const getRandomThoughts = (int) => {
+        const results = [];
+        for (let i = 0; i < int; i++) {
+            results.push({
+                thoughtText: getRandomArrItem(thoughtTexts),
+                username: getRandomArrItem(names),
+                reactions: [
+                    {
+                        reactionBody: getRandomArrItem(reactions),
+                        username: getRandomArrItem(names),
+                    },
+                ],
+            });
+        }
+        return results;
+    };
+
+    const getRandomName = () =>
+        `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+
+    const getThoughReactions = (int) => {
+        const results = [];
+        for (let i = 0; i < int; i++) {
+            results.push({
+                reactionBody: getRandomArrItem(reactions),
+                username: getRandomArrItem(names),
+            });
+        }
+        return results;
+    };
+
+
+    module.exports = { getRandomName, getRandomThoughts };
+        
---
+++ utils/seed.js
@@ -0,0 +1,44 @@
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+const { getRandomName, getRandomThoughts } = require("./data");
+
+connection.on("error", (err) => err);
+
+connection.once("open", async () => {
+  console.log("connected");
+
+  // Drop existing users
+  await User.deleteMany({});
+
+  // Drop existing thoughts
+  await Thought.deleteMany({});
+
+  // Create empty array to hold the users
+  const users = [];
+  const friends = [];
+
+  // Loop 20 times -- add users to the users array
+  for (let i = 0; i < 20; i++) {
+    // Get some random thought objects using a helper function that we imported from ./data
+    const thoughts = getRandomThoughts(20);
+
+    const username = getRandomName();
+    const email = `${username.replace(/ /g, "")}@email.com`
+
+    users.push({
+      username,
+      email,
+    });
+  }
+
+  // Add users to the collection and await the results
+  await User.collection.insertMany(users);
+  // Add thoughts to the collection and await the results
+  await Thought.collection.insertMany(thoughts);
+
+  // Log out the seed data to indicate what should appear in the database
+  console.table(users);
+  console.table(thoughts);
+  console.info("Seeding complete! 🌱");
+  process.exit(0);
+});
